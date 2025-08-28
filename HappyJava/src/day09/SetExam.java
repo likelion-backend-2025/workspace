1d2b4f32c74ec472d16df39557b261666bbbcd42
carami <carami@nate.com>
2025-08-28 14:07:59 +0900

    update exam

--- HappyJava/src/day09/SetExam.java
+++ HappyJava/src/day09/SetExam.java
@@ -1,6 +1,7 @@
 package day09;
 
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 
 public class SetExam {
@@ -27,5 +28,21 @@ public class SetExam {
         penSet.add(new Pen("red"));
 
         System.out.println(penSet);
+//set은 index를 갖고있지 않다.  순서가 중요하지 않은 자료구조.
+        System.out.println();
+
+        Iterator<String> iterator = fruitSet.iterator();
+        while(iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+        System.out.println("================");
+        for(String str : fruitSet){
+            System.out.println(str);
+        }
+
+        System.out.println(fruitSet.contains("사과"));
+        System.out.println(fruitSet.remove("사과"));
+        System.out.println(fruitSet);
+        System.out.println(fruitSet.size());
     }
 }

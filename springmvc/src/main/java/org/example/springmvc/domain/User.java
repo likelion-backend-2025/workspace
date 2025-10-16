e39d720b2f020cb297a5073b58c2319d935bbc9c
carami <carami@nate.com>
2025-10-16 17:43:44 +0900

    update exam

--- springmvc/src/main/java/org/example/springmvc/domain/User.java
+++ springmvc/src/main/java/org/example/springmvc/domain/User.java
@@ -1,6 +1,10 @@
 package org.example.springmvc.domain;
 
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,10 +15,18 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class User {
+    @NotEmpty(message = "이름을 입력하세요.")
     private String name;
+    @NotEmpty(message = "아디이를 입력하세요.")
     private String username;
+    @NotEmpty(message = "이메일을 입력하세요.")
+    @Email(message = "이메일 형식에 맞게 작성하세요.")
     private String email;
     private boolean admin;
+    @NotEmpty(message = "비밀번호를 입력하세요.")
+    @Size(min = 4, max = 6, message = "비밀번호는 4-6 자 사이만 입력됩니다.")
+//    @Pattern(regexp = ".*[!@#$%^&*(),.?\":{}|<>].*",message = "비밀번호는 최소 하나의 특수문자를 포함해야만 합니다. ")
+    private String password;
 
 
 }

fbffd4b2d981348c1175d1c042e2b440c82e78ba
carami <carami@nate.com>
2025-10-29 15:53:57 +0900

    update exam

--- restexam/src/main/java/org/example/restexam/controller/UserController.java
+++ restexam/src/main/java/org/example/restexam/controller/UserController.java
@@ -1,36 +1,45 @@
 package org.example.restexam.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.example.restexam.domain.User;
+import org.example.restexam.service.UserService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-//@RequestMapping("/users")
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
 public class UserController {
-    //user에 대해서 crud 할수있는 메서드를 정의해 볼 수 있겠죠??
-    ///users   -- get
-    @GetMapping("/users")
-    public List<User> getUsers(){
+    private final UserService userService;
 
-        return null;
-    }
-    @GetMapping("/users/{id}")
-    public User getUser(@PathVariable Long id){
-        return null;
-    }
+    @GetMapping
+    public ResponseEntity<List<User>> getUsers(){
 
-    @PostMapping("/users")
-    public User addUser(@RequestBody User user){
-        return null;
+        return ResponseEntity.ok(userService.getUsers());
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUser(@PathVariable("id") Long id){
+
+        return ResponseEntity.ok(userService.getUser(id));
     }
 
-    @PutMapping("/users/{id}")
-    public User updateUser(@RequestBody User user){
-        return null;
+    @PostMapping
+    public ResponseEntity<User> addUser(@RequestBody User user){
+        User createUser = userService.addUser(user);
+        return ResponseEntity.ok(createUser);
     }
-    @DeleteMapping("/users/{id}")
-    public void deleteUser(){
 
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable("id")Long id,@RequestBody User user){
+        user.setId(id);
+        User updateUser = userService.updateUser(user);
+        return ResponseEntity.ok(updateUser);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable("id")Long id){
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
     }
 }
---
+++ restexam/src/main/java/org/example/restexam/domain/Product.java
@@ -0,0 +1,12 @@
+package org.example.restexam.domain;
+
+public class Product {
+    private Long id;
+    private String name;
+    private int price;
+
+}
+//Product 클래스를 기준으로,  아래 제시된 부분을 만들어서..  완성해보세요.
+//엔티티, repository, service, controller,  .http  까지 작성하실수 있으실 거예요^^
+
+//UserController - > 응답을 좀 더 세심하게 생성해주세요.

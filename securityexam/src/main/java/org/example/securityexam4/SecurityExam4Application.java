c85d65dfdf9ca6453e6ebc0fced71b914ba72627
carami <carami@nate.com>
2025-11-05 13:52:24 +0900

    update exam

---
+++ securityexam/src/main/java/org/example/securityexam4/SecurityExam4Application.java
@@ -0,0 +1,11 @@
+package org.example.securityexam4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityExam4Application {
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityExam4Application.class, args);
+    }
+}
